55cc93a428848e707c36c3378fd581277dd8009b
Badru Ndawula <badrundawula24@gmail.com>
2026-09-20 19:45:35 +0300

    Cleanup accidentally tracked logs again

--- scripts/find-errors.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const content = fs.readFileSync("super-linter-push.log", "utf8");
-const clean = content.replace(/\x1B\[[0-9;]*[a-zA-Z]/g, "");
-const lines = clean.split("\n");
-const errors = lines.filter((l) => l.toLowerCase().includes("error"));
-errors.slice(-50).forEach((e) => console.log(e));
